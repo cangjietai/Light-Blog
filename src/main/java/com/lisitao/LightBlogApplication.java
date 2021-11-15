fc1f4fe126e6fde8ece4e789359b18ec912e9a1a
cangjietai <1296172553@qq.com>
2021-11-15 08:31:02 +0800

    1-添加前端代码

---
+++ src/main/java/com/lisitao/LightBlogApplication.java
@@ -0,0 +1,13 @@
+package com.lisitao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LightBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lisitao/LightBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lisitao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
